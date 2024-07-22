aab4c71f5c4426dcb450dff8cfc3c4afeef3b952
shv-px <shv@purestorage.com>
2024-07-22 16:52:31 +0530

    Spelling mistake corrected

--- src/service/googlefonts.service.spec.ts
+++ src/service/googlefonts.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { GooglefontsService } from './googlefonts.service';
+import { GoogleFontsService } from './googlefonts.service';
 
 describe('GooglefontsService', () => {
-  let service: GooglefontsService;
+  let service: GoogleFontsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(GooglefontsService);
+    service = TestBed.inject(GoogleFontsService);
   });
 
   it('should be created', () => {
